b77bd6ba5bff045386ae9769d5aa9c4c886e2a54
Felipe V. Rodrigues <felipe@felipevr.com>
2018-11-30 08:27:55 -0200

    Add benchmark for append versus insert
    I've read on Python for Data Analysis[1] that append is computationaly
    faster than insert. That's a claim that I have heard a few times on the
    past but never actually tested it.
    
    It turns out that the claim is very much true. Append is indeed faster.
    
    [1] McKinney Wes. 2018. Python for Data Analysis (2 ed.). O'Reilly Media, Inc.

---
+++ append-vs-insert/__main__.py
@@ -0,0 +1,39 @@
+"""
+Which one is faster?
+
+l = []
+l.append(1)
+
+or
+
+l = []
+l.insert(len(l), 1)
+"""
+from sys import argv
+from time import perf_counter
+
+def main(iteractions=1000000):
+
+    l = []
+    start = perf_counter()
+
+    for i in range(iteractions):
+        l.append(i)
+
+    finish = perf_counter()
+    print("Append of {0} items took {1:.4f}s".format(iteractions, finish - start))
+
+    l = []
+    start = perf_counter()
+
+    for i in range(iteractions):
+        l.insert(i, i)
+
+    finish = perf_counter()
+    print("Insert of {0} items took {1:.4f}s".format(iteractions, finish - start))
+
+if __name__ == "__main__":
+    if len(argv) > 1 and argv[1].isnumeric():
+        main(iteractions=int(argv[1]))
+    else:
+        main()
